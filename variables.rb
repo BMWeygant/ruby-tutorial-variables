a46db40def88903c3a283b70680a68af5118637f
BMWeygant <bobbydigital108@gmail.com>
2020-07-10 22:22:43 -0400

    Added solution

--- variables.rb
+++ variables.rb
@@ -1,1 +1,10 @@
 #Code your solution here.
+money = "$45"
+
+making = "I made"
+
+making_money = "#{making} #{money} today!"
+
+# Alternate solution:
+
+# making_money = making + " " + money + " " + "today!"
